30d232d10face79cc3bf7799a7d200bd95839793
hhjunls <1344842907@qq.com>
2026-01-13 15:21:15 +0800

    Add files via upload

---
+++ 5.2/student.cpp
@@ -0,0 +1,17 @@
+//student.cpp                     在此文件中进行函数的定义
+#include <iostream>
+#include"student.h"   //不要漏写此行，否则编译通不过
+#pragma warning(disable : 4996)
+using namespace std;
+void Student::display()         //在类外定义display类函数
+{
+    cout << "num:" << num << endl;
+    cout << "name:" << name << endl;
+    cout << "sex:" << sex << endl;
+}
+void Student::set_value(int n, const char* na, char s)
+{
+    num = n;
+    strcpy(name, na);
+    sex = s;
+}
